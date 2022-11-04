8c49084f9f5e02f7aa6e0fc5d63c0b0c8e07373d
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-11-04 22:07:08 +0100

    tralingSlash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // for non-organization GitHub Pages repos (projectName not ending in .github.io).
   // Otherwise, this needs to be explicit as a config field or environment variable.
   deploymentBranch: 'gh-pages',
-
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -76,7 +76,7 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/JordiNodeJS',
             label: 'GitHub',
             position: 'right',
           },
@@ -103,11 +103,11 @@ const config = {
               },
               {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://github.com/JordiNodeJS',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com',
               },
             ],
           },
